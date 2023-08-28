12ea7c3a3ad28d50c0c0c74ee958934953c5aa74
Irfan Maulana <irfan.resimen2018@gmail.com>
2023-08-29 00:05:52 +0700

    feat: encrypt room with sqlchiper

--- core/src/main/java/id/irfanrev/vegefood/core/di/LocalDataSourceModule.kt
+++ core/src/main/java/id/irfanrev/vegefood/core/di/LocalDataSourceModule.kt
@@ -3,6 +3,8 @@ package id.irfanrev.vegefood.core.di
 import androidx.room.Room
 import id.irfanrev.vegefood.core.data.source.local.LocalDataSource
 import id.irfanrev.vegefood.core.data.source.local.room.MealsDatabase
+import net.sqlcipher.database.SQLiteDatabase
+import net.sqlcipher.database.SupportFactory
 import org.koin.android.ext.koin.androidContext
 import org.koin.dsl.module
 
@@ -10,9 +12,14 @@ val localDataSourceModule = module {
     factory { get<MealsDatabase>().mealsDao() }
     single { LocalDataSource(get()) }
     single {
+        val passphrase: ByteArray = SQLiteDatabase.getBytes("Skills39".toCharArray())
+        val factory = SupportFactory(passphrase)
         Room.databaseBuilder(
             androidContext(),
             MealsDatabase::class.java, "meals.db"
-        ).fallbackToDestructiveMigration().build()
+        )
+            .fallbackToDestructiveMigration()
+            .openHelperFactory(factory)
+            .build()
     }
 }
